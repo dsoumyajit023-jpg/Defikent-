61ca44dfb1b6dc0b04a5784f38f265db363268f5
Soumyajit.D <dsoumyajit023@gmail.com>
2026-05-09 13:56:21 +0530

    Create saveData.js

---
+++ Defikent/api/saveData.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  // Handle save logic here
+  res.status(200).json({ success: true });
+}
